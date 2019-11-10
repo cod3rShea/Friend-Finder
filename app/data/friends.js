20e4ebf3a8389df56da670d36783088973356055
Sean Michael Shea <seanshea1337@gmail.com>
2019-11-10 16:36:45 -0500

    add user to array

--- app/data/friends.js
+++ app/data/friends.js
@@ -18,31 +18,31 @@ var potenialFriends = [
         "name": "Joey Weedo",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
             2,
+            4,
             5,
             4,
-            1
+            2,
+            1,
+            4,
+            5,
+            4,
+            2
         ]
     }, {
         "name": "Shelbey Surgent",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [
-            5,
-            1,
+            3,
+            2,
+            2,
+            2,
             4,
-            4,
-            5,
             1,
             2,
             5,
             4,
-            1
+            4
         ]
     },
 ];
